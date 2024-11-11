c1d58fec5bad7c9adafa0c5f0b440c5f1c66f0fc
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-11-11 16:28:08 +0500

    task completed

--- src/lesson1/Lesson5.java
+++ src/lesson1/Lesson5.java
@@ -8,17 +8,35 @@ import java.util.Map;
 public class Lesson5 {
 
     private static Map<Integer, Integer> map = new HashMap<>();
+    private static int sum = 0;
 
     public static void main(String[] args) {
 
         // Факториал
-        /*System.out.println(factorial(0));
+        System.out.println(factorial(0));
         System.out.println(factorial(-12));
         System.out.println(factorial(5));
-        System.out.println(factorial(8));*/
+        System.out.println(factorial(8));
 
         // Фибоначи
         System.out.println(fibonacci(6));
+
+        // Сумма чисел
+        System.out.println(sum(10));
+        System.out.println(sum(1));
+
+        // СТепень
+        System.out.println(degree(2,3));
+        System.out.println(degree(2,1));
+        System.out.println(degree(2,0));
+
+        // Палиндром
+        System.out.println(isPalindrome("level"));
+        System.out.println(isPalindrome(""));
+        System.out.println(isPalindrome("a"));
+        System.out.println(isPalindrome("azamat"));
+        System.out.println(isPalindrome("root"));
+        System.out.println(isPalindrome("qwfgwq"));
     }
 
     // 1. Факториал числа
@@ -35,9 +53,11 @@ public class Lesson5 {
     }
 
     // 2. Числа Фибоначчи
-    public static int fibonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+    private static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
 
         // если мы уже вычисляли, то возвращаем
         if (map.containsKey(n)) {
@@ -49,5 +69,44 @@ public class Lesson5 {
         map.put(n, result);
 
         return result;
+    }
+
+    // 3. Сумма чисел от 1 до n
+    private static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        // декремент и добавляю все числа
+        return n + sum(n - 1);
+    }
+
+    // 4. Возведение числа в степень
+    private static int degree(int n, int m) {
+        // елси степень равен нуллю то возваращаю 1
+        if (m == 0) {
+            return 1;
+        } else if (m == 1) {
+            return n; // если степень единица - возвращаю n
+        }
+
+        // умножаю число на декремент степени
+        return n * degree(n, m - 1);
+    }
+
+    // 5. Палиндром
+    public static boolean isPalindrome(String s) {
+        // если размер равен единице или нуллю - то это палнидром
+        if (s.length() <= 1) {
+            return true;
+        }
+
+        // если после рекурсий элементы не равны, то это не палиндром
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+
+        // сравниваю перый и последний элемент, если при каждой итерации находим палиндром - перехожу на следующие элементы
+        return isPalindrome(s.substring(1, s.length() - 1));
     }
 }
